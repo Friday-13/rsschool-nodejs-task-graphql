b72382e400a67590bbd63bb29af6c9e37eeb0825
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-22 17:23:04 +0300

    feat: implement queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import {
+  GraphQLBoolean,
   GraphQLEnumType,
   GraphQLFloat,
   GraphQLInt,
@@ -37,6 +38,10 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   });
 };
 
+const uuidType = new GraphQLScalarType({
+  name: 'UUID',
+});
+
 const memberTypeId = new GraphQLEnumType({
   name: 'MemberTypeId',
   values: {
@@ -66,7 +71,7 @@ const postType = new GraphQLObjectType({
   description: "User's post",
   fields: () => ({
     id: {
-      type: new GraphQLNonNull(GraphQLString),
+      type: new GraphQLNonNull(uuidType),
     },
     title: {
       type: new GraphQLNonNull(GraphQLString),
@@ -77,10 +82,105 @@ const postType = new GraphQLObjectType({
   }),
 });
 
+const profileType = new GraphQLObjectType({
+  name: 'Profile',
+  description: 'Profile',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(uuidType),
+    },
+    isMale: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+    yearOfBirth: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    memberType: {
+      type: new GraphQLNonNull(memberType),
+      resolve: async (profile, _args, context: PrismaClient) => {
+        const memberType = await context.memberType.findUnique({
+          where: {
+            id: profile.memberTypeId,
+          },
+        });
+        return memberType;
+      },
+    },
+  }),
+});
+
+const userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'User',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(uuidType),
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    balance: {
+      type: new GraphQLNonNull(GraphQLFloat),
+    },
+    profile: {
+      type: profileType,
+      resolve: async (user, _args, context: PrismaClient) => {
+        const profile = await context.profile.findUnique({
+          where: {
+            userId: user.id,
+          },
+        });
+        return profile;
+      },
+    },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(postType))),
+      resolve: async (user, _args, context: PrismaClient) => {
+        const posts = context.post.findMany({
+          where: {
+            authorId: user.id,
+          },
+        });
+        return posts;
+      },
+    },
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(userType))),
+      resolve: async (user, _args, context: PrismaClient) => {
+        const users = context.user.findMany({
+          where: {
+            subscribedToUser: {
+              some: {
+                subscriberId: user.id,
+              },
+            },
+          },
+        });
+        return users;
+      },
+    },
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(userType))),
+      resolve: async (user, _args, context: PrismaClient) => {
+        const users = context.user.findMany({
+          where: {
+            userSubscribedTo: {
+              some: {
+                authorId: user.id,
+              },
+            },
+          },
+        });
+        return users;
+      },
+    },
+  }),
+});
+
 const queryType = new GraphQLObjectType({
-  name: 'Query',
+  name: 'query',
   fields: {
-    postType: {
+    post: {
       type: postType,
       resolve: async (_source, { id }: { id: string }, context: PrismaClient) => {
         const post = await context.post.findUnique({
@@ -92,16 +192,15 @@ const queryType = new GraphQLObjectType({
       },
       args: {
         id: {
-          type: GraphQLString,
+          type: new GraphQLNonNull(uuidType),
           description: 'Post id',
         },
       },
     },
-    postsType: {
+    posts: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(postType))),
       resolve: async (_source, _args, context: PrismaClient) => {
         const posts = await context.post.findMany();
-        console.log(posts);
         return posts;
       },
     },
@@ -109,7 +208,6 @@ const queryType = new GraphQLObjectType({
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(memberType))),
       resolve: async (_source, _args, context: PrismaClient) => {
         const memberTypes = await context.memberType.findMany();
-        console.log(memberTypes);
         return memberTypes;
       },
     },
@@ -134,12 +232,62 @@ const queryType = new GraphQLObjectType({
         },
       },
     },
+    profiles: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(profileType))),
+      resolve: async (_source, _args, context: PrismaClient) => {
+        const profiles = await context.profile.findMany();
+        return profiles;
+      },
+    },
+    profile: {
+      type: profileType,
+      resolve: async (_source, { id }: { id: string }, context: PrismaClient) => {
+        const profile = await context.profile.findUnique({
+          where: {
+            id: id,
+          },
+        });
+        return profile;
+      },
+      args: {
+        id: {
+          type: new GraphQLNonNull(uuidType),
+          description: 'Profile uuid',
+        },
+      },
+    },
+
+    users: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(userType))),
+      resolve: async (_source, _args, context: PrismaClient) => {
+        const users = context.user.findMany();
+        return users;
+      },
+    },
+
+    user: {
+      type: userType,
+      resolve: async (_source, { id }: { id: string }, context: PrismaClient) => {
+        const user = context.user.findUnique({
+          where: {
+            id: id,
+          },
+        });
+        return user;
+      },
+      args: {
+        id: {
+          type: new GraphQLNonNull(uuidType),
+          description: 'User uuid',
+        },
+      },
+    },
   },
 });
 
 const schema = new GraphQLSchema({
   query: queryType,
-  types: [postType, memberType],
+  types: [postType, memberType, profileType, userType],
 });
 
 export default plugin;
