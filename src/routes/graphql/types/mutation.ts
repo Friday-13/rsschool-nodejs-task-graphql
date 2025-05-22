5ba618a855076474d8b115ecb1fb29861dc507a5
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-22 23:24:56 +0300

    feat: add createProfile mutation

--- src/routes/graphql/types/mutation.ts
+++ src/routes/graphql/types/mutation.ts
@@ -1,6 +1,8 @@
 import {
+  GraphQLBoolean,
   GraphQLFloat,
   GraphQLInputObjectType,
+  GraphQLInt,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLString,
@@ -9,6 +11,10 @@ import userType from './user.js';
 import { PrismaClient } from '@prisma/client';
 import { Static } from '@sinclair/typebox';
 import { createUserSchema } from '../../users/schemas.js';
+import profileType from './profile.js';
+import { UUIDType } from './uuid.js';
+import { memberTypeId } from './member.js';
+import { createProfileSchema } from '../../profiles/schemas.js';
 
 type TCreateUser = Static<typeof createUserSchema.body>;
 
@@ -20,6 +26,26 @@ const createUserInputType = new GraphQLInputObjectType({
     },
     balance: {
       type: new GraphQLNonNull(GraphQLFloat),
+    },
+  },
+});
+
+type TCreateProfile = Static<typeof createProfileSchema.body>;
+
+const createProfileInputType = new GraphQLInputObjectType({
+  name: 'CreateProfileInput',
+  fields: {
+    isMale: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+    yearOfBirth: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    userId: {
+      type: new GraphQLNonNull(UUIDType),
+    },
+    memberTypeId: {
+      type: new GraphQLNonNull(memberTypeId),
     },
   },
 });
@@ -45,6 +71,21 @@ const mutationType = new GraphQLObjectType({
       args: {
         dto: {
           type: createUserInputType,
+        },
+      },
+    },
+
+    createProfile: {
+      type: new GraphQLNonNull(profileType),
+      resolve: async (_source, { dto }: { dto: TCreateProfile }, context) => {
+        const profile = context.profile.create({
+          data: dto,
+        });
+        return profile;
+      },
+      args: {
+        dto: {
+          type: createProfileInputType,
         },
       },
     },
